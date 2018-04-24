cdfa281ea9e45db58be71a51fe3380678bd72893
Justus von der Beek <justusvonderbeekgmail.com>
2018-04-24 13:37:29 +0200

    First unfinished implementation of the binary search. TODO: the search for the interval boundaries isn't working fine. should be fixed, as well as testing the function a bit more.

---
+++ src/Blatt2/BinarySearchTesting.java
@@ -0,0 +1,35 @@
+package Blatt2;
+
+public class BinarySearchTesting {
+
+	public static void main(String[] args) {
+		// TODO: implement testing here
+		int[] sortedData = {-33, -5, -2, 0, 3, 7, 15, 33 ,122};
+		int[] secondSortedData = {-10, 33, 50, 99, 123, 4242};
+		boolean flag = true;
+		int test = BinSea.search(sortedData, -4, flag);
+		int test2 = BinSea.search(secondSortedData, -20, flag);
+		if (flag) {
+			if (test != -1) {
+				System.out.println("Untere Intervallgrenze in sortedData: " + test + " Zahl: " + sortedData[test]);
+			} 
+			if (test2 != -1) {
+				System.out.println("Untere Intervallgrenze in secondSortedData: " + test2 + " Zahl: " + secondSortedData[test2]);
+			}
+		} else {
+			if (test != -1) {
+				System.out.println("Obere Intervallgrenze in sortedData: " + test + " Zahl: " + sortedData[test]);
+			}
+			if (test2 != -1) {
+				System.out.println("Obere Intervallgrenze in secondSortedData: " + test2 + " Zahl: " + secondSortedData[test2]);
+			}
+			
+			
+		}
+		
+		Interval interval = BinSea.search(sortedData, new NonEmptyInterval(10, 50));
+		Interval interval2 = BinSea.search(secondSortedData, new NonEmptyInterval(10, 50));
+		System.out.println("Gefundenes Interval:\nsortedData: " + interval.toString() + "\nsecondSortedData: " + interval2.toString());
+		
+	}
+}
