7da11f6e08cc0c3cb13c87fe811e07c21e33c544
Justus <Justus@Justus-PC>
2018-05-04 14:15:10 +0200

    Hoffentlich klappt jetzt alles

--- src/Blatt3/StackyQueue.java
+++ src/Blatt3/StackyQueue.java
@@ -7,11 +7,12 @@ package Blatt3;
  * Datenspeicherung verwendet werden.
  */
 public class StackyQueue {
-  private DynamicArray waiting;
-  private DynamicArray working;
+  private DynamicStack waiting;
+  private DynamicStack working;
 
   private NonEmptyInterval waitingUsage;
   private NonEmptyInterval workingUsage;
+ 
   private int length;
   /**
    * Diese Methode ermittelt die Länge der Warteschlange.
@@ -28,8 +29,8 @@ public class StackyQueue {
    * @param maxOverhead
    */
   public StackyQueue (int growthFactor, int maxOverhead) {
-	waiting = new DynamicArray(growthFactor, maxOverhead);
-	working = new DynamicArray(growthFactor, maxOverhead);
+	waiting = new DynamicStack(growthFactor, maxOverhead);
+	working = new DynamicStack(growthFactor, maxOverhead);
 	this.length = 0;
 	waitingUsage = new NonEmptyInterval(0,0);
 	workingUsage = new NonEmptyInterval(0,0);
@@ -40,12 +41,10 @@ public class StackyQueue {
    * 
    * @param value der einzufügende Wert
    */
+  
   public void enqueue (int value) {
     length++;
-	waitingUsage = new NonEmptyInterval(waitingUsage.getFrom(), waitingUsage.getTo()+1);
-	waitingUsage = (NonEmptyInterval) waiting.reportUsage(waitingUsage, getLength());
-	waiting.set(waitingUsage.getTo(), value);
-	System.out.println(this);
+    waiting.pushBack(value);
   }
   
   /**
@@ -55,27 +54,18 @@ public class StackyQueue {
    */
   public int dequeue () {
      length--;
-	 if (workingUsage.getSize(working.getInnerLength()) == 0) {
-		for (int i = 0; i < waitingUsage.getSize(waiting.getInnerLength()); i++) {
-			if (waitingUsage.getSize(waiting.getInnerLength()) > 0) {
-				waitingUsage = new NonEmptyInterval(waitingUsage.getFrom(), waitingUsage.getTo()-1);
-			} else {
-				break;
-			}
-			int value = waiting.get(waitingUsage.getTo());
-			waitingUsage = new NonEmptyInterval(waitingUsage.getFrom(), waitingUsage.getTo()-1);
-			waiting.reportUsage(waitingUsage, waitingUsage.getSize(waiting.getInnerLength()));
-			workingUsage = new NonEmptyInterval(workingUsage.getFrom(), workingUsage.getTo());
-			working.reportUsage(workingUsage, workingUsage.getSize(working.getInnerLength()));
+	 if (working.getLength() == 0) {
+		 for (int i = 0; i <= getLength(); i++) {
+			int value = waiting.popBack();
+			working.pushBack(value);
 		}
 	 }
-	 workingUsage = (NonEmptyInterval) working.reportUsage(workingUsage, workingUsage.getSize(working.getInnerLength()));
-	 int value = working.get(workingUsage.getTo());
+	 int value = working.popBack();
 	 return value;
   }
   
   public String toString() {
-	  String result = "Noch nicht implementiert";
+	  String result = "\t" + waiting + "\n\t" + working;
 	  return result;
   }
   
