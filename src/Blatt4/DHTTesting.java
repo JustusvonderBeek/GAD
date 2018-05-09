29839d7e15dad3cb03d13053860ca8b88b6615a5
Justus von der Beek <justusvonderbeek@gmail.com>
2018-05-09 17:06:47 +0200

    Bearbeitet: HashString, Testing
    Fixed die letzten Fehler und hinzufügen von weiteren Testcases.

--- src/Blatt4/DHTTesting.java
+++ src/Blatt4/DHTTesting.java
@@ -5,28 +5,64 @@ public class DHTTesting {
 	public static void main(String[] args) {
 		DHT table = new DHT(7, 269); // initialisieren eine DHT mit 7 (Primzahl) Stores à 269 Slots (Primzahl)
 		
-		int itteration = 20;
+		System.out.println(table.hashKey("test"));
+		System.out.println(table.hashKey("test"));
+		System.out.println(table.hashKey("TEST"));
+		System.out.println(table.hashKey("TEST"));
+		System.out.println(table.hashKey("Test"));
+		System.out.println(table.hashKey("Test"));
+		System.out.println(table.hashKey("tesT"));
+		System.out.println(table.hashKey("tesT"));
+		System.out.println(table.hashKey(" "));
+		System.out.println(table.hashKey(" "));
+		System.out.println(table.hashKey("Das ist ein sehr sehr langer String der hoffentlich alles kaputt macht"));
+		System.out.println(table.hashKey("Das ist ein sehr sehr langer String der hoffentlich alles kaputt macht"));
+		System.out.println(table.hashKey("DasistimmernocheinsuperlangerTextMITGANZEplötzlichauftauchendenSachenWIEKLEinundGrtjkasgtbja,sbgkjashgjklashglkahsglkhalksghalkhglkahsljkgfhalkshglkashglkashlkghaslkghajklshvjlkashglkjahgaklshglkashglk"));
+		System.out.println(table.hashKey("DasistimmernocheinsuperlangerTextMITGANZEplötzlichauftauchendenSachenWIEKLEinundGrtjkasgtbja,sbgkjashgjklashglkahsglkhalksghalkhglkahsljkgfhalkshglkashglkashlkghaslkghajklshvjlkashglkjahgaklshglkashglk"));
+		System.out.println(table.hashKey("12345"));
+		System.out.println(table.hashKey("12345"));
 		
-		HashString hs = new HashString(20);
+		System.out.println("---------------");
 		
-//		hs.hash("testT");
-//		hs.hash("zweiterTest");
-//		hs.hash("blablabla");
-//		hs.hash("nocheinTessts");
-//		
-//		hs.hash("testT");
-//		hs.hash("zweiterTest");
-//		hs.hash("blablabla");
-//		hs.hash("nocheinTessts");
+		DHT table2 = new DHT(55, 2692); // initialisieren eine zweite DHT mit anderen Werten
 		
-		table.hashKey("test");
+		System.out.println(table2.hashKey("test"));
+		System.out.println(table2.hashKey("test"));
+		System.out.println(table2.hashKey("TEST"));
+		System.out.println(table2.hashKey("TEST"));
+		System.out.println(table2.hashKey("Test"));
+		System.out.println(table2.hashKey("Test"));
+		System.out.println(table2.hashKey("tesT"));
+		System.out.println(table2.hashKey("tesT"));
+		System.out.println(table2.hashKey(" "));
+		System.out.println(table2.hashKey(" "));
+		System.out.println(table2.hashKey("Das ist ein sehr sehr langer String der hoffentlich alles kaputt macht"));
+		System.out.println(table2.hashKey("Das ist ein sehr sehr langer String der hoffentlich alles kaputt macht"));
+		System.out.println(table2.hashKey("DasistimmernocheinsuperlangerTextMITGANZEplötzlichauftauchendenSachenWIEKLEinundGrtjkasgtbja,sbgkjashgjklashglkahsglkhalksghalkhglkahsljkgfhalkshglkashglkashlkghaslkghajklshvjlkashglkjahgaklshglkashglk"));
+		System.out.println(table2.hashKey("DasistimmernocheinsuperlangerTextMITGANZEplötzlichauftauchendenSachenWIEKLEinundGrtjkasgtbja,sbgkjashgjklashglkahsglkhalksghalkhglkahsljkgfhalkshglkashglkashlkghaslkghajklshvjlkashglkjahgaklshglkashglk"));
+		System.out.println(table.hashKey("12345"));
+		System.out.println(table.hashKey("12345"));
 		
-//		String test = "";j
-//		
-//		for (int i = 0; i < itteration; i++) {
-//			test += "Test";
-//			System.out.println("Key: " + table.hashKey(test));
-//		}
+		System.out.println("---------------");
+		
+		DHT table3 = new DHT(2, 50); // initialisieren eine dritte DHT mit anderen Werten
+		
+		System.out.println(table3.hashKey("test"));
+		System.out.println(table3.hashKey("test"));
+		System.out.println(table3.hashKey("TEST"));
+		System.out.println(table3.hashKey("TEST"));
+		System.out.println(table3.hashKey("Test"));
+		System.out.println(table3.hashKey("Test"));
+		System.out.println(table3.hashKey("tesT"));
+		System.out.println(table3.hashKey("tesT"));
+		System.out.println(table3.hashKey(" "));
+		System.out.println(table3.hashKey(" "));
+		System.out.println(table3.hashKey("Das ist ein sehr sehr langer String der hoffentlich alles kaputt macht"));
+		System.out.println(table3.hashKey("Das ist ein sehr sehr langer String der hoffentlich alles kaputt macht"));
+		System.out.println(table3.hashKey("DasistimmernocheinsuperlangerTextMITGANZEplötzlichauftauchendenSachenWIEKLEinundGrtjkasgtbja,sbgkjashgjklashglkahsglkhalksghalkhglkahsljkgfhalkshglkashglkashlkghaslkghajklshvjlkashglkjahgaklshglkashglk"));
+		System.out.println(table3.hashKey("DasistimmernocheinsuperlangerTextMITGANZEplötzlichauftauchendenSachenWIEKLEinundGrtjkasgtbja,sbgkjashgjklashglkahsglkhalksghalkhglkahsljkgfhalkshglkashglkashlkghaslkghajklshvjlkashglkjahgaklshglkashglk"));
+		System.out.println(table.hashKey("12345"));
+		System.out.println(table.hashKey("12345"));
 		
 	}
 
--- src/Blatt4/HashString.java
+++ src/Blatt4/HashString.java
@@ -6,7 +6,10 @@ package Blatt4;
  */
 public class HashString {
   private int m;
-
+  private int w;
+  private int k;
+  private int[] a;
+  
   /**
    * Dieser Konstruktor initialisiert ein {@link HashString}
    * Objekt für einen gegebenen Maximalwert (size - 1) der gehashten
@@ -16,6 +19,9 @@ public class HashString {
    */
   public HashString (int size) {
     this.m = size;
+    this.w = (int) (Math.log(size)/Math.log(2));		// Ausrechnen, wie viele Bits in einen Teil gehören
+    this.k = 32;										// Da k abhängig von der eingehenden Bitlänge ist, können wir k hier noch garnicht wissen und ich nehme deshalb einfach das Maximum an
+    initilizeA();
   }
 
   /**
@@ -27,49 +33,40 @@ public class HashString {
   
   public int hash (String key) {
 	  String bitstring= processKey(key);
-	  int w = (int) (Math.log(m)/Math.log(2));		// Ausrechnen, wie viele Bits in einen Teil gehören
 	  int k = bitstring.length()/w;					// Ausrechnen der Anzahl an Teilen
 	  int[] parts = new int[k];						// Erstellen des k-Tupels
-	  
-	  // TODO: muss den Bitstring noch umdrehen, damit ich ihn einfacher bearbeiten kann. vllt auch in byteToInteger
 	  createParts(bitstring, parts, w);				// Überführen der einzelnen Bytes in ein k-Tupel
-	  int[] a = new int[k];
-	  for (int i = 0; i < a.length; i++) {
-		a[i] = i*2+5;
-	  }
 	  int result = 0;
 	  for (int i = 0; i < parts.length; i++) {
 		result += parts[i] * a[i];
 	  }
 	  result %= m;
-	  System.out.println("String: " + key + "\nByteValue: " + bitstring + "\nW: " + w + "\nK: " + k + "\nArray: " + printingArray(parts) + "\nErgebnis: " + result);
+//	  System.out.println("String: " + key + "\nByteValue: " + bitstring + "\nW: " + w + "\nK: " + k + "\nArray: " + printingArray(parts) + "\nErgebnis: " + result + "\n-----------------------------------");
 	  return result;
   }
   
   private String processKey(String key) {
-	  String hash = Integer.toBinaryString(key.hashCode()); // Erstellen eines Integerwertes aus dem String Schlüssel
-	  int hexValue = Integer.toHexString(key.hashCode()).length() * 4; // Vergleichswert zum Auffüllen mit Nullen
+	  String hash = Integer.toBinaryString(key.hashCode()); 			// Erstellen eines Integerwertes aus dem String Schlüssel
+	  int hexValue = Integer.toHexString(key.hashCode()).length() * 4; 	// Vergleichswert zum Auffüllen mit Nullen
 	  if (hash.length() != hexValue) {
 		  int difference = hexValue - hash.length();
 		  String add = "";
 		  for (int i = 0; i < difference; i++) {
-			add += "0";								// Einfädeln von führenden Nullen solange, bis die Differenz ausgeglichen ist
+			add += "0";													// Einfädeln von führenden Nullen solange, bis die Differenz ausgeglichen ist
 		  }
-		  add += hash;								// Anhängen des ursprünglichen Strings, sodass eine Korrekte Bitdarstellung gewährleistet ist
-		  hash = add;							// Ändern der Referenz
+		  add += hash;													// Anhängen des ursprünglichen Strings, sodass eine Korrekte Bitdarstellung gewährleistet ist
+		  hash = add;													// Ändern der Referenz
 	  }
-	  System.out.println("Result: " + hash);
+//	  System.out.println("Result: " + hash);
 	  return hash;
   }
   
   private void createParts(String string, int[] parts, int w) {
 	  int lowerIndex = 0;
 	  int higherIndex = w;
-	  System.out.println("Parts " + parts.length);
 	  for (int i = 0; i < parts.length; i++) {
-		 String substring = string.substring(lowerIndex, higherIndex);
-		 System.out.println("Substring: " + substring);
-		 parts[i] = byteToInteger(substring);
+		 String substring = string.substring(lowerIndex, higherIndex);	// Erzeugen von Substrings
+		 parts[i] = byteToInteger(substring);							// Einfügen der vorher berechneten w Bitwerte in das Array
 		 lowerIndex = higherIndex;
 		 higherIndex += w;
 	  }
@@ -77,14 +74,24 @@ public class HashString {
   
   private int byteToInteger(String string) {
 	  int result = 0;
-	  int index = 0;
+	  int index = string.length();
 	  for (int i = 0; i < string.length(); i++) {
-		result += Integer.parseInt(string.substring(index, ++index))*Math.pow(2, i);
+		result += Integer.parseInt(string.substring(index-1, index))*Math.pow(2, i);	// Die Umsetzung des Binärsystems; zur Implementierung: da der String die Elemente "falsch" herum hält, beginne ich beim obersten Element und arbeite mich nach unten durch
+		index--;
 	  }
 	  return result;
   }
   
-  private String printingArray(int[] array) {
+  private int[] initilizeA() {
+	  a = new int[k];
+	  for (int i = 0; i < a.length; i++) {
+		a[i] = (int) (Math.random()*m) + 1;
+	  }
+	  return a;
+  }
+  
+  // Zu Debugzwecken
+  private String printingArray(int[] array) { 
 	  String result = "";
 	  for (int i = 0; i < array.length; i++) {
 		result += array[i] + "; ";
