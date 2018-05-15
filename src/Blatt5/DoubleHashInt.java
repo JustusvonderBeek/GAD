a7469ace46240a5c9f4aae1024428a88c0fcf403
ge69hig <ge69hig@DESKTOP-KE96U65>
2018-05-15 12:12:48 +0200

    Kleine Änderung an der DoubleHashInt

--- src/Blatt5/DoubleHashInt.java
+++ src/Blatt5/DoubleHashInt.java
@@ -7,9 +7,11 @@ import java.util.Random;
  * Integer zu hashen.
  */
 public class DoubleHashInt implements DoubleHashable<Integer> {
-  /*
-   * Todo
-   */
+ 
+	int m;
+	int k;
+	int w;
+	int[] a;
 
   /**
    * Dieser Konstruktor initialisiert ein {@link DoubleHashInt}
@@ -19,9 +21,10 @@ public class DoubleHashInt implements DoubleHashable<Integer> {
    * @param size die Größe der Hashtabelle
    */
   public DoubleHashInt (int size) {
-    /*
-     * Todo
-     */
+	  this.m = size;
+	  this.w = (int) (Math.log(size)/Math.log(2));
+	  this.k = 32;
+	  initilizeA();
   }
 
   /**
@@ -31,9 +34,8 @@ public class DoubleHashInt implements DoubleHashable<Integer> {
    * @return der Hashwert des Schlüssels
    */
   @Override public long hash (Integer key) {
-    /*
-     * Todo
-     */
+    int hashCode = key.hashCode();
+    
   }
 
   /**
@@ -46,6 +48,26 @@ public class DoubleHashInt implements DoubleHashable<Integer> {
     /*
      * Todo
      */
+  }
+  
+  private String printingDebug() {
+	  String result = "Size: " + this.m + "\n";
+	  result += "K: " + this.k + "\n";
+	  result += "W: " + this.w + "\n";
+	  result += "A: ";
+	  for (int i = 0; i < a.length; i++) {
+		result += i + ": " + a[i] + "; ";
+	  }
+	  result += "\n";
+	  return result;
+	  
+  }
+  
+  private void initilizeA() {
+	  this.a = new int[k];
+	  for (int i = 0; i < a.length; i++) {
+		this.a[i] = (int) (Math.random()*m) + 1;
+	  }
   }
 
 }
